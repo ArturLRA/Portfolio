9dd929b40e110bd822f739c3c85b3adc489a6b06
ArturLRA <artur.luiz.rodrigues.alves@gmail.com>
2025-11-19 22:45:56 -0300

    third commit

--- Index.js
+++ Index.js
@@ -2,9 +2,9 @@ window.addEventListener('scroll', () => {
     const navBar = document.querySelector('.navBar');
 
     if (window.scrollY > 0) {
-        navBar.classList.add('shadow'); 
+        navBar.classList.add('shadow');
     } else {
-        navBar.classList.remove('shadow'); 
+        navBar.classList.remove('shadow');
     }
 });
 
